a1b961054aac8e1af663baa02b4e2280542815c1
Juan Miñaca <jminaca@espol.edu.ec>
2025-07-30 16:28:16 -0500

    2 Primeros Metodos
    resueltos

--- src/test/java/calculatorsmp/OperationsTest.java
+++ src/test/java/calculatorsmp/OperationsTest.java
@@ -4,10 +4,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 
-/**
- *
- * @author ajdelgado
- */
+
 public class OperationsTest {
 
+
+    @Test
+    @DisplayName("Probar Metodo MakeFormula")
+    public void CM1() {
+        String result = Operations.MakeFormula();
+        assertNotNull(result);
+        assertTrue(result.matches("\\d+([+\\-*/]\\d+)+"));
+    }
+
+
+    @Test
+    @DisplayName("Test Verificador Sumatoria")
+    public void CM2() {
+        String formula = "33+77";
+        String expResult = "33+77=110";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+    }
 }
