43d10789e59b2b40d5ee7e4fa648aaef21ccefab
Adrian Delgado <git@adrianjdelgado.com>
2025-07-30 15:49:01 -0500

    feat: crear archivo de test

---
+++ src/test/java/calculatorsmp/OperationsTest.java
@@ -0,0 +1,13 @@
+package calculatorsmp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+
+/**
+ *
+ * @author ajdelgado
+ */
+public class OperationsTest {
+
+}
